1dc2ba8aec24a837822905643f4e85b9855b1dfe
buhron <mbh90180@gmail.com>
2025-01-30 17:18:58 -0500

    Update install.sh

--- installlers/install.sh
+++ installlers/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-
-curl https://buhron.github.io/upack-pkgs/pkgs/
+mkdir $UPACK_PREFIX
+# Fetch the non-existent bootstrap archives...
+curl https://buhron.gtihub.io/upack-pkgs/pkgs/bootstrap/archive/bootstrap-1.0.0-$(uname -m).zip -#L -o /tmp/upack/bootstrap.zip
 # Post-installation messages
 if [ $? = "0" ]; then
  printf '[✅] Upack successfully installed!\n'
