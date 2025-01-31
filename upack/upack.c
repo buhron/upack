3d3543a1605652b7529bd5702b109c1f7455450a
buhron <mbh90180@gmail.com>
2025-01-31 14:49:03 -0500

    deleted:    upack/upack 	modified:   upack/upack.c 	modified:   upack/upack.c 	upack/upack

--- upack/upack.c
+++ upack/upack.c
@@ -5,10 +5,13 @@
 #include <json-c/json.h>
 #include <zip.h>
 int main(int action, char *args[]) {
+    CURL *curl;
+    CURLcode res;
     char *prefix = getenv("UPACK_PREFIX");
+    curl = curl_easy_init();
     curl_global_init(CURL_GLOBAL_ALL);
     if (strcmp(args[1], "update") == 0 ) {
-      printf("Note: i need to fix the package servers so the update action is unavailable\n");
+                  
       return 1;
     }
     printf("Usage: upack <action> <args>\nupack install - install a package\nupack update - update the package database\n");
