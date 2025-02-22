f5336ae49b637a6b96bc48545065c165cef93694
buhron <mbh90180@gmail.com>
2025-02-22 12:05:05 -0500

    Update install.sh

--- installlers/install.sh
+++ installlers/install.sh
@@ -8,10 +8,10 @@ echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$UPACK_PREFIX/lib:$UPACK_PREFIX/li
 mkdir $UPACK_PREFIX
 # Fetch the non-existent bootstrap archives...
 echo "Downloading upack archive..."
-curl https://buhron.github.io/upack-pkgs/pkgs/upack/upack-1.0.0-$(uname -m).zip -#L -o /tmp/upack/bootstrap.zip
+curl https://buhron.github.io/upack-pkgs/pkgs/upack/upack-1.0.0-$(uname -m).zip -#L -o $TMPDIR/upack/bootstrap.zip
 # long command to extract bootstrap and delete all the upack-package specific stuff that comes with it
 echo "Extracting bootstrap archive..."
-cd $UPACK_PREFIX; unzip /tmp/upack/bootstrap.zip > /dev/null 2>&1; rm $UPACK_PREFIX/upack.cfg > /dev/null 2>&1; cp -r $UPACK_PREFIX/data/* $UPACK_PREFIX; rm -rf $UPACK_PREFIX/data
+cd $UPACK_PREFIX; unzip $TMPDIR/upack/bootstrap.zip > /dev/null 2>&1; rm $UPACK_PREFIX/upack.cfg > /dev/null 2>&1; cp -r $UPACK_PREFIX/data/* $UPACK_PREFIX; rm -rf $UPACK_PREFIX/data
 # is upack installed? if yes, great! youre ready!
 which upack > /dev/null 2>&1
 # chmod 777 the upack directory because everyone deserves a chance to isntall upack packages :)
